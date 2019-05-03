a8b480e9c3110cb015e3f6cee5ec4140096f1ac9
Amol Chopra <achopra@wundermancommerce.com>
2019-05-03 15:48:58 +0530

    Updates

---
+++ precache-manifest.d71b86056c0804637f08bec053e5d86a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05fdec282e2ef15049db764da001765",
+    "url": "/lyrics-finder/index.html"
+  },
+  {
+    "revision": "db57dedf6f93e7f80442",
+    "url": "/lyrics-finder/static/css/main.61accc46.chunk.css"
+  },
+  {
+    "revision": "d3a7276c9733fd1e0aae",
+    "url": "/lyrics-finder/static/js/2.30b6eba7.chunk.js"
+  },
+  {
+    "revision": "db57dedf6f93e7f80442",
+    "url": "/lyrics-finder/static/js/main.106925e5.chunk.js"
+  },
+  {
+    "revision": "5ec1a9ceb339fe7f25e4",
+    "url": "/lyrics-finder/static/js/runtime~main.af84d037.js"
+  }
+]);
